1710c445b3050730691580283761da196e9ba59a
Thanh Nguyen <viechoctap6789@gmail.com>
2024-08-24 19:32:41 +0700

    ch5

---
+++ src/components/Admin/SideBar.js
@@ -0,0 +1,94 @@
+import 'react-pro-sidebar/dist/css/styles.css';
+import {
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SubMenu,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from 'react-pro-sidebar';
+
+import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart} from 'react-icons/fa';
+import sidebarBg from '../../assets/bg2.jpg';
+import { IoLogoReact } from "react-icons/io5";
+import { MdDashboard } from "react-icons/md";
+import './SideBar.scss'
+
+
+
+const SideBar = (props) => {
+    const { image, collapsed, toggled, handleToggleSidebar } = props;
+    return (
+        <>
+            <ProSidebar
+                image={sidebarBg}
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onToggle={handleToggleSidebar}
+            >
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        <IoLogoReact size={'2em'} color={'00bfff'}/>
+                        <span>Thanh Nguyen</span>
+                    </div>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        <MenuItem
+                            icon={<MdDashboard />}
+                        >
+                            Dashboard
+                        </MenuItem>
+                    </Menu>
+                    <Menu iconShape="circle">
+                        <SubMenu
+                            icon={<FaGem />}
+                            title='Features'
+                        >
+                            <MenuItem> Quản lý Users</MenuItem>
+                            <MenuItem> Quản lý bài Quiz</MenuItem>
+                            <MenuItem> Quản lý câu hỏi</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </SidebarContent>
+
+                <SidebarFooter style={{ textAlign: 'center' }}>
+                    <div
+                        className="sidebar-btn-wrapper"
+                        style={{
+                            padding: '20px 24px',
+                        }}
+                    >
+                        <a
+                            // href="https://github.com/azouaoui-med/react-pro-sidebar"
+                            target="_blank"
+                            className="sidebar-btn"
+                            rel="noopener noreferrer"
+                        >
+                            <FaGithub />
+                            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                Thanh Nguyen
+                            </span>
+                        </a>
+                    </div>
+                </SidebarFooter>
+            </ProSidebar>
+        </>
+    )
+}
+
+export default SideBar;
